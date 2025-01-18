901da628898add026ec7f16a34be56d4278c8b4e
iminparallel <ch.harit1995@gmail.com>
2025-01-18 17:54:01 +0530

    changed footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -15,8 +15,8 @@ const Footer = () => {
         boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)", // Optional: Adds a shadow
       }}
     >
-      <small className="inline-block bg-gray-300 hover:bg-gray-800 text-black font-semibold py-2 px-4 rounded transition duration-300">
-        Harit Chowdhury
+      <small className="inline-block bg-gray-300 hover:bg-gray-800 text-black font-semibold py-2 px-2 rounded transition duration-300">
+        DocWhisperer@2025
       </small>
     </footer>
   );
