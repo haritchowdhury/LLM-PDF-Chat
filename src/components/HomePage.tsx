56f91afbc64641a15d0de9aec142d82f4fa394de
Harit <ch.harit1995@gmail.com>
2025-04-17 11:49:05 +0530

    removed telegram group

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -75,12 +75,12 @@ const LandingPage = () => {
         </Link>
         <br />
         <br />
-        <Link
+        {/* <Link
           href={`https://t.me/+CRAp6PqTVA40MjM1`}
           className="bg-yellow-200 hover:bg-yellow-700 text-black font-medium py-2 px-2 rounded"
         >
           Join Beta Users
-        </Link>
+        </Link> */}
 
         <p className="text-xl md:text-2xl mb-2 max-w-3xl mx-auto pt-6 pb-6">
           Or organize your research with our personal work-spaces.
